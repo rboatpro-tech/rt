0421dedf866783980e73c1b679831b4f1ed00201
rboatpro-tech <rboatpro@gmail.com>
2026-04-18 16:54:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,47 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    maxHttpBufferSize: 1e7
-});
-const mongoose = require('mongoose'); // Подключаем базу
+const io = require('socket.io')(http, { maxHttpBufferSize: 1e7 });
+const mongoose = require('mongoose');
 
-// 1. Берем ссылку из переменных окружения Render
 const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI).then(() => console.log('База Алмаз готова к группам!'));
 
-// 2. Подключаемся к MongoDB
-if (mongoURI) {
-    mongoose.connect(mongoURI)
-        .then(() => console.log('База данных Алмаз подключена!'))
-        .catch(err => console.error('Ошибка базы данных:', err));
-} else {
-    console.log('ВНИМАНИЕ: Переменная MONGODB_URI не найдена в настройках Render!');
-}
-
-// 3. Создаем схему хранения сообщений
 const messageSchema = new mongoose.Schema({
+    room: String, // Новое поле для группы
     user: String,
     text: String,
-    image: String, // Для картинок
+    image: String,
     time: { type: Date, default: Date.now }
 });
 const Message = mongoose.model('Message', messageSchema);
 
 app.use(express.static(__dirname + '/www'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+io.on('connection', (socket) => {
+    // Когда пользователь выбирает группу
+    socket.on('join room', async (data) => {
+        socket.join(data.room);
+        console.log(`${data.user} зашел в группу: ${data.room}`);
 
-io.on('connection', async (socket) => {
-    console.log('Новое подключение к Алмазу');
-
-    // 4. ПРИ ВХОДЕ: Отправляем старые сообщения из базы
-    try {
-        const history = await Message.find().sort({ time: -1 }).limit(50);
+        // Загружаем историю только для этой группы
+        const history = await Message.find({ room: data.room }).sort({ time: -1 }).limit(50);
         socket.emit('chat history', history.reverse());
-    } catch (err) {
-        console.error('Не удалось загрузить историю:', err);
-    }
+    });
 
     socket.on('chat message', async (msg) => {
-        // 5. СОХРАНЯЕМ сообщение в базу
-        try {
-            const newMessage = new Message({
-                user: msg.user,
-                text: msg.text,
-                image: msg.image // если отправляете картинки в msg
-            });
-            await newMessage.save();
-            
-            // 6. Рассылаем всем
-            io.emit('chat message', msg);
-        } catch (err) {
-            console.error('Ошибка сохранения сообщения:', err);
-        }
+        const newMessage = new Message({
+            room: msg.room, // Сохраняем, в какую группу ушло сообщение
+            user: msg.user,
+            text: msg.text,
+            image: msg.image
+        });
+        await newMessage.save();
+        
+        // Отправляем сообщение ТОЛЬКО участникам этой группы
+        io.to(msg.room).emit('chat message', msg);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, '0.0.0.0', () => {
-    console.log(`Сервер Алмаз запущен на порту: ${PORT}`);
-});
+http.listen(PORT, '0.0.0.0', () => console.log(`Сервер Алмаз на порту ${PORT}`));
